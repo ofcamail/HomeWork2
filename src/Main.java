097b8c5b987c9919d82a87fb403f8a19b9fa5d4d
ofcmail <ofcmailkom@gmail.com>
2022-12-02 18:50:06 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int a1 = 1;
-        int b1 = 2;
+        int a1 = 4;
+        int b1 = 12;
         int a2 = 1;
-        int b2 = 4;
-boolean isIntersectionPointa2Andb1 = (a1<a2 && b1<b2 && (a2-a1) <a1 && (b2-b1) < b1);
-boolean isIntersectionPointa1Andb2 = (a2>a1 && b1>b2 && (a1-a2) <a2 && (b1-b2) < b2);
-boolean isIntersectionPointa2Andb2 = (a1<=a2 && b1>=b2);
-boolean isIntersectionPointa1Andb1 = (a2<=a1 && b2>=b1);
+        int b2 = 8;
+boolean isIntersectionPointa2Andb1 = (a1<a2 || a1==a2 && b1<b2 || b1==b2);
+boolean isIntersectionPointa1Andb2 = (a1>a2 || a1==a2 && b1>b2 || b1==b2);
+boolean isIntersectionPointa2Andb2 = (a1<a2 || a1==a2 && b1>b2 || b1==b2);
+boolean isIntersectionPointa1Andb1 = (a1>a2 || a1==a2 && b1<b2 || b1==b2);
 boolean isIntersectionPointa1 = (a1==b2);
 boolean isIntersectionPointa2 = (a2==b1);
 if (isIntersectionPointa2Andb1) {
